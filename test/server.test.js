dc79e6802ba35ac779a590699b89c090a10beab8
Anatoliy Chakkaev <mail@anatoliy.in>
2016-06-26 17:15:20 +0100

    Test context save / restore

--- test/server.test.js
+++ test/server.test.js
@@ -4,6 +4,19 @@ const expect = require('expect');
 const queueTransport = require('../');
 const rabbitUrl = process.env.RABBIT_URL || 'amqp://192.168.99.101:5672';
 
+// mimic ORM
+const jobs = [];
+
+function Job(data) {
+    jobs.push(Object.assign(this, data, { id: jobs.length + 1 }));
+}
+
+Job.create = data => Promise.resolve(new Job(data));
+Job.find = id => {
+    const job = jobs.find(j => j.id === Number(id));
+    return Promise.resolve(job);
+};
+
 describe('server', () => {
 
     let transport;
@@ -20,7 +33,8 @@ describe('server', () => {
                     exchange: 'task',
                     routes: [ 'command' ]
                 }
-            }
+            },
+            getContextId: context => Job.create({ context }).then(job => String(job.id))
         });
 
         transport.server({
@@ -54,9 +68,16 @@ describe('server', () => {
                 }
             },
             handler: {
-                result: res => result1 = res,
-                error: err => result2 = err
-            }
+                result: res => {
+                    console.log('result handler called');
+                    result1 = res;
+                },
+                error: err => {
+                    console.log('error handler called');
+                    result2 = err;
+                }
+            },
+            getContextById: contextId => Job.find(contextId).then(job => job && job.context)
         });
 
         return transport.getReady();
@@ -66,19 +87,19 @@ describe('server', () => {
     after(() => transport.close());
 
     it('can produce results asyncronously', (done) => {
-        client(1);
+        client(1, null, { context: { say: 'hello' }});
         setTimeout(() => {
             expect(result1).toEqual('hola');
             done();
         }, 300);
     });
 
-    it('can produce results asyncronously', (done) => {
+    it('can produce errors asyncronously', (done) => {
         client(0);
         setTimeout(() => {
             expect(result2.message).toEqual('Oops');
             done();
-        }, 300);
+        }, 500);
     });
 
 });
