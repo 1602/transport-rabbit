fbcfa951d4140bbb8fe0cd6e26c7793dfd2ea477
Anatoliy Chakkaev <mail@anatoliy.in>
2016-07-05 14:59:52 +0100

    Added pending channel wrapper tests

---
+++ test/channel.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('channel', () => {
+
+    it('should send messages to queue');
+
+    it('should publish messages to exchange');
+
+    it('should allow to take care of stream throughput');
+
+});
+
