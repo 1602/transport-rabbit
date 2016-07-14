06a21154a8e21f0a1a931954479c02840a8c8826
Anatoliy Chakkaev <mail@anatoliy.in>
2016-07-14 20:14:52 +0100

    Added consumer tests

---
+++ test/consumer.test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expect = require('expect');
+const queueTransport = require('../');
+const rabbitUrl = process.env.RABBIT_URL || 'amqp://192.168.99.101:5672';
+
+describe.only('consumer', () => {
+
+    let transport = null;
+
+    beforeEach(() => transport = queueTransport({
+        url: rabbitUrl
+    }));
+
+    afterEach(() => transport.close());
+
+    it('should consume queues', () => {
+        transport.consumer({
+            channelName: 'custom',
+            exchangeName: 'task',
+            queueName: 'command'
+        });
+
+        let queueConsumed = false;
+        const chan = transport.getChannel('custom');
+        chan.consume = () => {
+            queueConsumed = true;
+            return Promise.resolve();
+        };
+
+        return transport.getReady()
+            .then(() => {
+                expect(queueConsumed).toBe(true);
+            });
+    });
+
+});
+
