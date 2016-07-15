cbf46096adbfb3531c8592d10f32e851df843782
Anatoliy Chakkaev <mail@anatoliy.in>
2016-07-15 17:18:57 +0100

    Remove unused helper

--- domains/helpers.js
+++ domains/helpers.js
@@ -1,19 +1,12 @@
 'use strict';
 
 module.exports = {
-    generateId,
-    getQueueName,
+    generateId
 };
 
 function generateId() {
     return Math.random().toString(36).substr(2) +
         Math.random().toString(36).substr(1) +
         Math.random().toString(36).substr(1);
-}
-
-function getQueueName(queueDescriptor) {
-    const exchange = queueDescriptor.exchange;
-    const route = queueDescriptor.routes[0];
-    return [ exchange, route ].join('.');
 }
 
